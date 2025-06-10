28692c0cef901be21033cd0bf7ba8f01b61b5618
Phongtrinh <113330202+PhongTrinhDinh@users.noreply.github.com>
2025-06-10 20:10:47 +0700

    Delete MI3060_Project directory

--- MI3060_Project/src/MedicalRecord.cpp
+++
@@ -1,7 +0,0 @@
-#include "..\include\MedicalRecord.hpp"
-
-MedicalRecord::MedicalRecord(const Time& arr, bool exam)
-    : arrivalTime(arr), examined(exam) {}
-
-MedicalRecord::MedicalRecord(const Time& arr, const Time& exam, const std::string& diag, const std::string& pres)
-    : arrivalTime(arr), examTime(exam), diagnosis(diag), prescription(pres), examined(true) {}
